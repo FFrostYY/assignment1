91a9da1f0dfcad461edfb74283e737df109910dd
Stribog <faticfilip98@gmail.com>
2017-09-06 16:37:45 +0000

    Version 0.4
    Added trim function. Put variables inside the menu loop so that they get reset each run, makes it safer to use. Replaced all s.next() with s.nextLine() in order to fix the trim function.

--- Assignment.java
+++ Assignment.java
@@ -4,28 +4,29 @@ public class Assignment {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);		//prepares program for input
 		char userInput = '\0';		//initializes input variable so it can be used in the loop
-		int j;
-		String s1 = "\0";
-		String s2 = "\0";
-		boolean check;	//initializes helper variables
 		while (userInput != 'q')	//loop that allows user to keep using the program until they want to quit
 		{
-			userInput = input.next().charAt(0);
+			int j;
+			String s1 = "\0";
+			String s2 = "\0";
+			boolean check;	//initializes helper variables
+			userInput = input.nextLine().charAt(0);
 			switch (userInput)						//checks which operation input corresponds to, prints error if it doesn't
 			{
 			case 'c':
 				System.out.println("You picked concatenation.");
-				System.out.println(" ");
 				System.out.println("Please enter the first string: ");
-				s1 = input.next();
+				s1 = input.nextLine();
 				System.out.println("Please enter the second string: ");
-				s2 = input.next();
+				s2 = input.nextLine();
 				System.out.println( "The result of the concatenating" + " " +  s1 + " " + "and" + " "  + s2 + " " + "is:" + " " + s1 + s2 + " " );
 			break;
 			case 'e':
 				System.out.println("You picked equals.");
-				s1 = input.next();
-				s2 = input.next();
+				System.out.println("Please enter the first string: ");
+				s1 = input.nextLine();
+				System.out.println("Please enter the second string: ");
+				s2 = input.nextLine();
 				j=0;
 				check = false;
 				if (s1.length() == s2.length())
@@ -60,6 +61,16 @@ public class Assignment {
 				break;
 			case 't':
 				System.out.println("You picked trim.");
+				System.out.println("Please enter a string to trim: ");
+				s1 = input.nextLine();
+				for(j=0; j < s1.length(); j++)
+				{
+					if (s1.charAt(j) != ' ')
+					{
+						s2 = s2 + s1.charAt(j);
+					}
+				}
+				System.out.println("The trimmed string is: " + s2);
 				break;
 			case 'l':
 				System.out.println("You picked lastIndexOf");
